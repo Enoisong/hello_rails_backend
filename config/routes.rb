c7b3a51d9dfd92f7b2bc6220e4ccdff365ad1c35
Enoisong <enobongitiaba988@gmail.com>
2023-10-04 09:40:02 -0700

    Add table for messages and seeds data

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  get "/random_greeting", to: "greetings#random_greeting"  
-  
+  get '/random_greeting', to: 'greetings#random_greeting'
 end
